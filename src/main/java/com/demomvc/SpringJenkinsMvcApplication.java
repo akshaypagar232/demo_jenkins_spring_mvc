93e8f0f03434c5217e70277adac888036e1055c0
akshaypagar232 <akshaypagar232@gmail.com>
2023-03-03 17:44:00 +0530

    jenkins project mvc

---
+++ src/main/java/com/demomvc/SpringJenkinsMvcApplication.java
@@ -0,0 +1,13 @@
+package com.demomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsMvcApplication.class, args);
+	}
+
+}
